0ebc903740f2ae8460feb0aeb445dd0cddca8f59
Jerry L Hoover <jerry.hoover@ymail.com>
2019-10-04 21:43:17 -0500

    Peer Submit

--- js/script.js
+++ js/script.js
@@ -3,45 +3,130 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+// =-=-=-=-=-=-=-=-= GLOBAL ARRAYS and VARIABLES =-=-=-=-=-=-=-=-=-=-=-=-=-=
+let quotes = [
+  {
+    quote: "I have not yet begun to code",
+    source: "Patrick Henry Jones",
+    citation: "Twitter",
+    year: "1775",
+    category: "Things people would have tweeted, if that was a thing"
+  },
+  {
+    quote: "You can't handle the truth",
+    source: "Jack Nicholson",
+    category: "Things people say in old movies"
+  },
+  {
+    quote: "tenpo suno ni o pona tawa sina",
+    source: "Me",
+    citation: "Just now... in Toki Pona",
+    category: "Things people say at conlang conventions"
+  },
+  {
+    quote: "Beagles unite!",
+    source: "Penny Jean the Beagle",
+    year: "2017",
+    category: "Things my dog would say... if she could talk"
+  },
+  {
+    quote: "You get a car, you get a car, you get a car...",
+    source: "Oprah",
+    citation: "The Oprah Winfrey Show",
+    category: "Things Oprah says when I'm not around"
+  },
+  {
+    quote: "Uh oh!",
+    source: "Your Doctor",
+    category: "Things your doctor shouldn't say"
+  }
+];
 
+// make interval visible so timer can be turned off
+var interval = false;
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function getRandomQuote() {
+  // generate a random number between 0 and the length of the array
+  let random = Math.floor(Math.random() * quotes.length);
 
+  //use random number to pull a quote
+  return quotes[random];
+}
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function printQuote() {
+  // declare variables
+  let hypertext = "";
+  let randomQuote = getRandomQuote();
 
+  // use template literals to drop the values into the string
+  hypertext = `<p class="quote">${randomQuote.quote}</p> <p class="source">${randomQuote.source}`;
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
+  // check for existence of citation
+  if (randomQuote.citation) {
+    // add citation to end of string
+    hypertext += `<span class="citation">${randomQuote.citation}</span>`;
+  }
 
+  // check for existence of year
+  if (randomQuote.year) {
+    hypertext += `<span class="year">${randomQuote.year}</span>`;
+  }
 
+  // add the category and close the final p tag
+  hypertext += `</p><p class="category">Category: ${randomQuote.category}</p>`;
 
+  // jam it into the DOM. Take that, DOM!
+  document.getElementById("quote-box").innerHTML = hypertext;
+  // change the background color to something still raedable
+  changeColor();
+}
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function changeColor() {
+  // an array of darkish colors
+  colors = [
+    "green",
+    "red",
+    "blue",
+    "orange",
+    "purple",
+    "maroon",
+    "steelblue",
+    "slategrey",
+    "sienna",
+    "saddlebrown",
+    "midnightblue",
+    "mediumblue",
+    "black",
+    "darkred",
+    "darkgreen",
+    "navy",
+    "chocolate"
+  ];
+  // generate a random number
+  let random = Math.floor(Math.random() * colors.length);
+  // use the random number to change the bg color
+  document.querySelector("body").style.backgroundColor = colors[random];
+}
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+function activateTimer() {
+  // get the state of the checkbox
+  checked = document.getElementById("chkTimer").checked;
+  notChecked = !checked;
 
+  if (checked) {
+    // change quote immediately, then change every ten seconds
+    printQuote();
+    interval = setInterval(printQuote, 10000);
+  }
 
+  if (notChecked) {
+    clearInterval(interval);
+  }
+}
 
 /***
   When the "Show another quote" button is clicked, the event listener 
@@ -50,7 +135,9 @@ project 1 - A Random Quote Generator
   comment.
 ***/
 
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+document
+  .getElementById("loadQuote")
+  .addEventListener("click", printQuote, false);
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+// eventlistener for checkbox
+document.getElementById("chkTimer").addEventListener("click", activateTimer);
